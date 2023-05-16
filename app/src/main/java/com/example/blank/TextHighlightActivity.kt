9d37421b9079dd565fd19014a2c93746e9128116
문다현 <moondda@moondda-MacBookAir.local>
2023-05-16 23:46:48 +0900

    feat:texthighlight_yellow

--- app/src/main/java/com/example/blank/TextHighlightActivity.kt
+++ app/src/main/java/com/example/blank/TextHighlightActivity.kt
@@ -1,17 +1,56 @@
 package com.example.blank
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.BackgroundColorSpan
+import android.widget.TextView
 
 class TextHighlightActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_text_highlight);
 
-        val intent = getIntent();
-        val OCRtext = intent.getStringExtra("text");
-        // OCRtext에 OCR한 text가 저장
+    private val textView: TextView by lazy {
+        findViewById(R.id.textView3)
     }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_highlight)
+
+
+//        val textView: TextView = findViewById(R.id.textView3)
+        val intent = intent
+        val ocrText = intent.getStringExtra("text")
+
+//        textView.text = ocrText
+        textView.text = "Hello World!"
+
+        textView.setOnLongClickListener {
+            val start = textView.selectionStart
+            val end = textView.selectionEnd
+
+            if (start >= 0 && end >= 0 && start != end) {
+                val text = textView.text
+                val builder = StringBuilder(text)
+                builder.replace(start, end, "")
+                textView.text = builder.toString()
+
+                // 텍스트 하이라이트 설정
+                val spannableString = SpannableString(textView.text)
+                spannableString.setSpan(
+                    BackgroundColorSpan(Color.YELLOW),
+                    start,
+                    end,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                textView.text = spannableString
+                true
+            } else {
+                false
+            }
+        }
+    }
+}
 
 //    private fun initMakeProblemButton() {
 //        makeProblem.setOnClickListener {
@@ -22,4 +61,3 @@ class TextHighlightActivity : AppCompatActivity() {
 //    }
 
 
-}
