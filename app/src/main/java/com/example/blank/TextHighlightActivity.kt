60c59596337bbe4ab0cfd0675c617184fa80bffd
Joontae <chlwnsxo0@gmail.com>
2023-06-13 01:22:55 +0900

    generate blank

--- app/src/main/java/com/example/blank/TextHighlightActivity.kt
+++ app/src/main/java/com/example/blank/TextHighlightActivity.kt
@@ -1,6 +1,5 @@
 package com.example.blank
 
-<<<<<<< HEAD
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
@@ -20,41 +19,19 @@ class TextHighlightActivity : AppCompatActivity() {
     }
 
     private val answerWordQueue: Queue<String> = LinkedList()
-=======
-import android.content.Intent
-import android.graphics.Color
-import android.os.Bundle
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.BackgroundColorSpan
-import android.widget.Button
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-
-class TextHighlightActivity : AppCompatActivity() {
-
-    private lateinit var textView: TextView
->>>>>>> 05007cf4ca6b471f332d1644078af32e52a6ae2c
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_highlight)
 
-<<<<<<< HEAD
         textView.text = "The sun rises in the east and sets in the west."
 
         initTextView()
     }
-=======
-        textView = findViewById(R.id.textView)
-        val intent = intent
-        val ocrText = intent.getStringExtra("text")
->>>>>>> 05007cf4ca6b471f332d1644078af32e52a6ae2c
 
     private fun initTextView() {
         textView.movementMethod = CustomLinkMovementMethod() // CustomLinkMovementMethod 설정
 
-<<<<<<< HEAD
         val text = textView.text.toString()
         val words = text.split(" ")
 
@@ -65,32 +42,6 @@ class TextHighlightActivity : AppCompatActivity() {
                     answerWordQueue.add(word) // 클릭한 단어를 answerWordQueue에 추가
                     replaceWordWithUnderscore(textView, word) // 단어를 _로 변경
                 }
-=======
-        textView.setOnLongClickListener {
-            textView.requestFocus()
-            val start = textView.selectionStart
-            val end = textView.selectionEnd
-
-            if (start >= 0 && end >= 0 && start != end) {
-                val text = textView.text
-                val builder = StringBuilder(text)
-                builder.replace(start, end, "")
-                textView.text = builder.toString()
-
-                // 텍스트 하이라이트 설정
-                val spannableString = SpannableString(textView.text)
-                spannableString.setSpan(
-                    BackgroundColorSpan(Color.YELLOW),
-                    start,
-                    end,
-                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                )
-                textView.text = spannableString
-                println(start)
-                true
-            } else {
-                false
->>>>>>> 05007cf4ca6b471f332d1644078af32e52a6ae2c
             }
 
             val wordStartIndex = text.indexOf(word)
@@ -130,10 +81,7 @@ class TextHighlightActivity : AppCompatActivity() {
             }
             return super.onTouchEvent(widget, buffer, event)
         }
-
-        initMakeProblemButton()
     }
-<<<<<<< HEAD
 }
 
 
@@ -149,15 +97,4 @@ class TextHighlightActivity : AppCompatActivity() {
 //            startActivity(intent)
 //        }
 //    }
-=======
->>>>>>> 05007cf4ca6b471f332d1644078af32e52a6ae2c
 
-    private fun initMakeProblemButton() {
-        val makeProblemButton: Button = findViewById(R.id.makeProblemButton)
-        makeProblemButton.setOnClickListener {
-            val intent = Intent(this, TextHighlightActivity::class.java)
-            intent.putExtra("text", textView.text.toString())
-            startActivity(intent)
-        }
-    }
-}
