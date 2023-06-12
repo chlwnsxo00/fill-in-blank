c344f9bbee4464ede7ea41973f9eb6aa75a81801
Joontae <chlwnsxo0@gmail.com>
2023-06-12 14:04:23 +0900

    camera ê기ëìa 기능 오류 수정

--- app/src/main/java/com/example/blank/PlayOCRActivity.kt
+++ app/src/main/java/com/example/blank/PlayOCRActivity.kt
@@ -2,10 +2,12 @@ package com.example.blank
 
 import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.view.Menu
@@ -14,11 +16,9 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -26,6 +26,10 @@ import com.example.ocr.TextRecognition
 import kotlinx.coroutines.*
 import java.io.File
 import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class PlayOCRActivity : AppCompatActivity() {
 
@@ -33,50 +37,60 @@ class PlayOCRActivity : AppCompatActivity() {
         findViewById(R.id.selectedImage)
     }
 
-    private val makeProblem: AppCompatButton by lazy{
+    private val makeProblem: AppCompatButton by lazy {
         findViewById(R.id.makeProblem)
     }
 
     private val OCRtext: TextView by lazy {
         findViewById(R.id.OCRtext)
     }
-
+    val REQUEST_TAKE_PHOTO = 1
+    var mCurrentPhotoPath: String? = null
     private var ocrJob: Job? = null // 현재 실행 중인 OCR 작업을 추적하기 위한 변수
     private val ocrScope = CoroutineScope(Dispatchers.Default + Job())
 
-    private var pickImageIntent =
+    private val takePhotoLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
-                try {
+                val data = result.data
+                val photoUri = when (data) {
+                    null -> {
+                        val photoFile = getPhotoFile()
+                        FileProvider.getUriForFile(this, "com.example.fileprovider", photoFile)
+                    }
+                    else -> {
+                        data.data
+                    }
+                }
 
-                    val uri: Uri = result.data?.data!!
+                try {
                     ocrJob?.cancel()
 
                     ocrJob = ocrScope.launch {
-                        val file = resizeImage(uri)
+                        val file = resizeImage(photoUri)
                         val image = selectedImage
                         launch(Dispatchers.Main) {
                             OCRtext.text = "                  --------분석 중----------"
-                            image.setImageURI(uri)
+                            image.setImageURI(photoUri)
                             image.scaleType = ImageView.ScaleType.CENTER_CROP
                         }
                         if (!isActive) return@launch
-                        TextRecognition.compressFile(this@PlayOCRActivity, file) {
-                            TextRecognition.fromFile(it, object : TextRecognition.Callback {
-                                override fun success(response: String) {
-                                    OCRtext.text = response
-                                }
+                        TextRecognition.compressFile(this@PlayOCRActivity, file) { compressedFile ->
+                            TextRecognition.fromFile(
+                                compressedFile,
+                                object : TextRecognition.Callback {
+                                    override fun success(response: String) {
+                                        OCRtext.text = response
+                                    }
 
-                                override fun failure(e: Throwable) {
-                                    OCRtext.text = e.toString()
-                                    Log.e("E", e.stackTraceToString())
-                                }
-                            })
+                                    override fun failure(e: Throwable) {
+                                        handleOCRFailure(e)
+                                    }
+                                })
                         }
                     }
                 } catch (e: Exception) {
-                    OCRtext.text = e.toString()
-                    Log.e("E",e.stackTraceToString())
+                    handleOCRFailure(e)
                 }
             }
         }
@@ -92,101 +106,180 @@ class PlayOCRActivity : AppCompatActivity() {
     private fun initMakeProblemButton() {
         makeProblem.setOnClickListener {
             intent = Intent(this, TextHighlightActivity::class.java)
-            intent.putExtra("text",OCRtext.text)
+            intent.putExtra("text", OCRtext.text)
             startActivity(intent)
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.top_menu_play_ocr_index, menu)
-        //R은 res 폴더의 약자. res폴더 안에 있는 context_menu_main.xml 파일과 연결시킨다.
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.take_photo -> {
-                // 사진찍기
-                when {
-                    ContextCompat.checkSelfPermission(
-                        this,
-                        android.Manifest.permission.CAMERA
-                    ) == PackageManager.PERMISSION_GRANTED -> {
-                        Toast.makeText(this, "권한 획득", Toast.LENGTH_SHORT).show()
-                        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-                            // Ensure that there's a camera activity to handle the intent
-                            takePictureIntent.resolveActivity(packageManager)
-                        }.also {
-                            pickImageIntent.launch(it)
-                        }
-                    }
-                    shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA) -> {
-                        Toast.makeText(this, "권한 거부", Toast.LENGTH_SHORT).show()
-                        showPermissionContextPopup()
-                    }
-                    else -> {
-                        Toast.makeText(this, "권한요청", Toast.LENGTH_SHORT).show()
-                        ActivityCompat.requestPermissions(
-                            this,
-                            arrayOf(android.Manifest.permission.CAMERA),
-                            1000
-                        )
-                    }
-                }
+                dispatchTakePictureIntent()
                 true
             }
             R.id.open_gallery -> {
-                // 갤러리 사진고르기
-                Intent().apply {
-                    type = "image/*"
-                    action = Intent.ACTION_GET_CONTENT
-                }.also {
-                    pickImageIntent.launch(it)
-                }
+                openGallery()
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
     }
 
-    private fun resizeImage(uri: Uri): File {
-        // Glide 이용해 고해상도 사진을 1MB 이하가 되기 위해 해상도를 줄임
-        // OCR 무료 버전에서 1MB 이하만 가능하기 때문
-        runOnUiThread(kotlinx.coroutines.Runnable {
-            OCRtext.text = "                    ------------이미지 리사이징 중------------ "
-        })
-
-        val requestBuilder = Glide.with(this)
-            .asBitmap()
-            .load(uri)
-            .format(DecodeFormat.PREFER_RGB_565)
-            .diskCacheStrategy(DiskCacheStrategy.NONE)
-            .skipMemoryCache(true)
-            .override(1024) // 원하는 크기로 설정
-            .submit()
-
-        val originalBitmap: Bitmap = requestBuilder.get()
-
-        originalBitmap?.let {
-            val resizedFile = File(cacheDir, "resized_image.jpg")
-            val outputStream = FileOutputStream(resizedFile)
-            it.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-            outputStream.flush()
-            outputStream.close()
-            return resizedFile
-        }
-        throw RuntimeException("Failed to resize the image.")
+    // 사진 촬영 후 썸네일만 띄워줌. 이미지를 파일로 저장해야 함
+    @Throws(IOException::class)
+    private fun createImageFile(): File? {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+            imageFileName,
+            ".jpg",
+            storageDir
+        )
+        mCurrentPhotoPath = image.absolutePath
+        return image
     }
 
-    private fun showPermissionContextPopup() {
-        AlertDialog.Builder(this)
-            .setTitle("권한이 필요합니다.")
-            .setMessage("카메라 사용을 위해 권한이 필요합니다.")
-            .setPositiveButton("동의하기") { _, _ ->
-                requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 1000)
+    // 카메라 인텐트 실행하는 부분
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            var photoFile: File? = null
+            try {
+                photoFile = createImageFile()
+            } catch (ex: IOException) {
             }
-            .setNegativeButton("취소하기") { _, _ -> }
-            .create()
-            .show()
+            if (photoFile != null) {
+                val photoURI =
+                    FileProvider.getUriForFile(this, "com.example.fileprovider", photoFile)
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+        try {
+            when (requestCode) {
+                REQUEST_TAKE_PHOTO -> {
+                    if (resultCode == RESULT_OK) {
+                        val file = File(mCurrentPhotoPath)
+                        val bitmap: Bitmap?
+                        if (Build.VERSION.SDK_INT >= 29) {
+                            val source: ImageDecoder.Source = ImageDecoder.createSource(
+                                contentResolver, Uri.fromFile(file)
+                            )
+                            try {
+                                bitmap = ImageDecoder.decodeBitmap(source)
+                                if (bitmap != null) {
+                                    selectedImage.setImageBitmap(bitmap)
+                                }
+
+                            } catch (e: IOException) {
+                                e.printStackTrace()
+                            }
+                        } else {
+                            try {
+                                bitmap = MediaStore.Images.Media.getBitmap(
+                                    contentResolver,
+                                    Uri.fromFile(file)
+                                )
+                                if (bitmap != null) {
+                                    selectedImage.setImageBitmap(bitmap)
+                                }
+                            } catch (e: IOException) {
+                                e.printStackTrace()
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (error: java.lang.Exception) {
+            error.printStackTrace()
+        }
+    }
+
+    private fun openGallery() {
+        Intent().apply {
+            type = "image/*"
+            action = Intent.ACTION_GET_CONTENT
+        }.also {
+            takePhotoLauncher.launch(it)
+        }
+    }
+
+    private fun getPhotoFile(): File {
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())}_",
+            ".jpg",
+            storageDir
+        )
+    }
+
+    private suspend fun resizeImage(uri: Uri?): File = withContext(Dispatchers.IO) {
+        runOnUiThread {
+            OCRtext.text = "--------이미지 리사이징 중--------"
+        }
+
+        uri?.let {
+            val requestBuilder = Glide.with(this@PlayOCRActivity)
+                .asBitmap()
+                .load(it)
+                .format(DecodeFormat.PREFER_RGB_565)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .skipMemoryCache(true)
+                .override(1024, 1024) // Desired size
+                .submit()
+
+            try {
+                val originalBitmap: Bitmap = requestBuilder.get()
+
+                originalBitmap?.let {
+                    val resizedFile = File(cacheDir, "resized_image.jpg")
+                    val outputStream = FileOutputStream(resizedFile)
+                    it.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                    outputStream.flush()
+                    outputStream.close()
+                    return@withContext resizedFile
+                }
+            } catch (e: Exception) {
+                handleResizeImageException(e)
+            }
+        }?: run {
+            // uri가 null인 경우 기본 공백 이미지를 처리하도록 수정합니다.
+            val bmp = Bitmap.createBitmap(1024, 1024, Bitmap.Config.RGB_565)
+            val resizedFile = File(cacheDir, "default_image.jpg")
+            val outputStream = FileOutputStream(resizedFile)
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            outputStream.flush()
+            outputStream.close()
+            return@withContext resizedFile
+        }
+
+        throw RuntimeException("The provided Uri is null.")
+    }
+
+
+    private fun handleResizeImageException(e: Exception) {
+        e.printStackTrace()
+        Log.e("E", e.stackTraceToString())
+        runOnUiThread {
+            Toast.makeText(this@PlayOCRActivity, "Failed to resize the image.", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+
+    private fun handleOCRFailure(e: Throwable) {
+        OCRtext.text = e.toString()
+        Log.e("E", e.stackTraceToString())
+        // Handle the OCR failure, log or display an error message.
     }
 }
