0e50a70e18c4d3437980a4296ea9fd4a07ba8875
Joontae <chlwnsxo0@gmail.com>
2023-06-13 01:12:41 +0900

    generate blank

--- app/src/main/java/com/example/blank/SplashActivity.kt
+++ app/src/main/java/com/example/blank/SplashActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import com.example.blank.R
 
 
 class SplashActivity : AppCompatActivity() {
@@ -17,7 +16,7 @@ class SplashActivity : AppCompatActivity() {
         Handler().postDelayed({
             // This method will be executed once the timer is over
             // Start your app main activity
-            startActivity(Intent(this, MainIndexActivity::class.java))
+            startActivity(Intent(this, TextHighlightActivity::class.java))
             // close this activity
             finish()
         }, splashTime)
--- app/src/main/java/com/example/blank/TextHighlightActivity.kt
+++ app/src/main/java/com/example/blank/TextHighlightActivity.kt
@@ -1,61 +1,98 @@
 package com.example.blank
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
-import android.text.style.BackgroundColorSpan
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.MotionEvent
+import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class TextHighlightActivity : AppCompatActivity() {
 
     private val textView: TextView by lazy {
         findViewById(R.id.textView)
     }
+
+    private val answerWordQueue: Queue<String> = LinkedList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_highlight)
 
+        textView.text = "The sun rises in the east and sets in the west."
 
-//        val textView: TextView = findViewById(R.id.textView3)
-        val intent = intent
-        val ocrText = intent.getStringExtra("text")
+        initTextView()
+    }
 
-        textView.text = ocrText
-        println(ocrText)
-        //     textView.text = "Hello World!"
+    private fun initTextView() {
+        textView.movementMethod = CustomLinkMovementMethod() // CustomLinkMovementMethod 설정
 
-        textView.setOnLongClickListener {
-            val start = textView.selectionStart
-            val end = textView.selectionEnd
+        val text = textView.text.toString()
+        val words = text.split(" ")
 
-            if (start >= 0 && end >= 0 && start != end) {
-                val text = textView.text
-                val builder = StringBuilder(text)
-                builder.replace(start, end, "")
-                textView.text = builder.toString()
-
-                // 텍스트 하이라이트 설정
-                val spannableString = SpannableString(textView.text)
-                spannableString.setSpan(
-                    BackgroundColorSpan(Color.YELLOW),
-                    start,
-                    end,
-                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                )
-                textView.text = spannableString
-                true
-            } else {
-                false
+        val spannableString = SpannableString(text)
+        for (word in words) {
+            val clickableSpan = object : ClickableSpan() {
+                override fun onClick(view: View) {
+                    answerWordQueue.add(word) // 클릭한 단어를 answerWordQueue에 추가
+                    replaceWordWithUnderscore(textView, word) // 단어를 _로 변경
+                }
             }
+
+            val wordStartIndex = text.indexOf(word)
+            val wordEndIndex = wordStartIndex + word.length
+            spannableString.setSpan(clickableSpan, wordStartIndex, wordEndIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+
+        textView.text = spannableString
+    }
+
+    // 선택된 단어를 단어의 알파벳 수만큼의 "_" 로 바꾸는 함수
+    private fun replaceWordWithUnderscore(textView: TextView, word: String) {
+        val text = textView.text.toString()
+        val underscoreWord = "_".repeat(word.length)
+        val replacedText = text.replace(word, underscoreWord)
+        textView.text = replacedText
+    }
+
+    // Custom MovementMethod 클래스
+    inner class CustomLinkMovementMethod : LinkMovementMethod() {
+        override fun onTouchEvent(widget: TextView, buffer: Spannable, event: MotionEvent): Boolean {
+            val action = event.action
+
+            if (action == MotionEvent.ACTION_UP) {
+                val x = event.x.toInt()
+                val y = event.y.toInt()
+
+                val layout = widget.layout
+                val line = layout.getLineForVertical(y)
+                val off = layout.getOffsetForHorizontal(line, x.toFloat())
+
+                val link = buffer.getSpans(off, off, ClickableSpan::class.java)
+                if (link.isNotEmpty()) {
+                    link[0].onClick(widget)
+                    return true
+                }
+            }
+            return super.onTouchEvent(widget, buffer, event)
         }
     }
 }
 
+
+//        val intent = intent
+//        val ocrText = intent.getStringExtra("text")
+//
+//        textView.text = ocrText
+
 //    private fun initMakeProblemButton() {
 //        makeProblem.setOnClickListener {
-//            intent = Intent(this, TextHighlightActivity::class.java)
+//            intent = Intent(this, com.example.blank.TextHighlightActivity::class.java)
 //            intent.putExtra("text",OCRtext.text)
 //            startActivity(intent)
 //        }
